cf34f30816ca7114146296405f6524f212d071f6
Joseph Jeremy C. Garcia <122107666+ZhayNaz@users.noreply.github.com>
2026-08-15 21:34:18 +0800

    99% done

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1200, // Raises the limit to 1.2 MB so the warning goes away
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
